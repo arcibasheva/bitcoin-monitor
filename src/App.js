c966c28b0dd534faf296237731996e3ea09e462d
arcibasheva <28alya@gmail.com>
2019-12-12 07:31:14 +0200

    Bitcoin monitor done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+            currentRate: {}
+        }
+    }
+
+    componentDidMount() {
+        this.getBPI().then((data) => {
+            this.setState({
+                loading: false,
+                currentRate: data,
+                prevRate: data
+            })
+        })
+    }
+
+    getBPI = async() => {
+        const url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+        const response = await fetch(url);
+        return await response.json()
+            .then((res) => {
+                return {
+                    EUR: res.bpi.EUR.rate_float,
+                    GBP: res.bpi.GBP.rate_float,
+                    USD: res.bpi.USD.rate_float,
+                }
+            });
+    }
+
+    refresh = () => {
+        this.getBPI().then((data) => {
+            this.setState(prevState => ({
+                loading: false,
+                currentRate: data,
+                prevRate: prevState.currentRate
+            }))
+        })
+    }
+
+    getDelte = (prev, current) => {
+        return (current - prev) > 0 ? "+" + (current - prev) : (current - prev)
+    }
+
+    render() {
+        const {loading, prevRate, currentRate} = this.state;
+
+        return <div>
+            {loading ? <div>Loading...</div> :
+                <table border="1">
+                    <thead>
+                    <tr>
+                        <th> </th>
+                        <th>Previous</th>
+                        <th>Current</th>
+                        <th>Delta</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th>&euro;</th>
+                        <td>{prevRate.EUR}</td>
+                        <td>{currentRate.EUR}</td>
+                        <td>{this.getDelte(prevRate.EUR,currentRate.EUR)}</td>
+                    </tr>
+                    <tr>
+                        <th>&pound;</th>
+                        <td>{prevRate.GBP}</td>
+                        <td>{currentRate.GBP}</td>
+                        <td>{this.getDelte(prevRate.GBP, currentRate.GBP)}</td>
+                    </tr>
+                    <tr>
+                        <th>&#36;</th>
+                        <td>{prevRate.USD}</td>
+                        <td>{currentRate.USD}</td>
+                        <td>{this.getDelte(prevRate.USD, currentRate.USD)}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            }
+            <button onClick={this.refresh}>Refresh</button>
+        </div>
+    }
+
 }
 
 export default App;
